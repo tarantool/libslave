573d70579f5efc8d76e97f78effb83e6cee1f38a
Sergey Bronnikov <sergeyb@tarantool.org>
2020-10-08 19:14:28 +0300

    test: enable tests on CI
    - Added script to execute `test_client`
    - Fixed SQL commands in a `db_filler.cpp`
    - Enabled unit test on CI with working testcases, broken testcases to be fixed
      in scope of #25
    - Added MySQL setup on CI before running tests
    
    Needed for #25
    Needed for #16

--- test/db_filler.cpp
+++ test/db_filler.cpp
@@ -118,12 +118,12 @@ int main(int argc, char** argv)
         createTable(opts, short_table, bulk_size, bulk_count,
                "CREATE TABLE " + short_table + "(\n"
                "id int NOT NULL auto_increment,\n"
-               "user_id int NOT NULL,\n"
-               "banner_template_id int NOT NULL,\n"
+               "user_id int NOT NULL DEFAULT 0,\n"
+               "banner_template_id int NOT NULL DEFAULT 0,\n"
                "PRIMARY KEY (id)\n"
                ")",
 
-               "INSERT INTO " + short_table + "(user_id, banner_template_id) values",
+               "INSERT INTO " + short_table + "(user_id, banner_template_id) VALUES",
                "(1, 2)",
                "UPDATE " + short_table + " SET user_id = 12"
             );
@@ -131,41 +131,41 @@ int main(int argc, char** argv)
     if (!long_table.empty())
         createTable(opts, long_table, bulk_size, bulk_count, "CREATE TABLE " + long_table + "(\n"
                "id int NOT NULL auto_increment,\n"
-               "created timestamp NOT NULL DEFAULT 0,\n"
+               "created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,\n"
                "updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\n"
                "status enum('active','blocked','deleted') NOT NULL DEFAULT 'active',\n"
                "system_status enum('active','blocked','deleted') NOT NULL DEFAULT 'active',\n"
 
                "alias varchar(255) NOT NULL,\n"
-               "user_id int NOT NULL,\n"
-               "banner_template_id int NOT NULL,\n"
-               "type_id text NOT NULL,\n"
+               "user_id int NOT NULL DEFAULT 0,\n"
+               "banner_template_id int NOT NULL DEFAULT 0,\n"
+               //"type_id text NOT NULL,\n"
                "flags set('default_dithering', 'console', 'ext_console', 'int_console', 'abstract', 'force_use_usergeo', 'filter_topics', 'filter_thematics', 'rtb', 'own_ctr', 'interface_only') NOT NULL DEFAULT 'default_dithering,own_ctr',\n"
                "banners_count int unsigned NOT NULL DEFAULT 2,\n"
                "shows_threshold_coeff double not null default 1.0,\n"
                "min_a_ratio double not null default 0.0,\n"
-               "cpm_limit int NOT NULL,\n"
-               "block_cpm_limit int NOT NULL,\n"
-               "a_block_cpm_limit int NOT NULL,\n"
-               "block_banners text NOT NULL,\n"
+               "cpm_limit int NOT NULL DEFAULT 0,\n"
+               "block_cpm_limit int NOT NULL DEFAULT 0,\n"
+               "a_block_cpm_limit int NOT NULL DEFAULT 0,\n"
+               //"block_banners text NOT NULL,\n"
 
-               "borrow_ctr_pad_id varchar(255) NOT NULL,\n"
-               "borrow_ctr_coeff varchar(255) NOT NULL,\n"
+               //"borrow_ctr_pad_id varchar(255) NOT NULL,\n"
+               //"borrow_ctr_coeff varchar(255) NOT NULL,\n"
                "borrow_ctr_no_ctr_action enum('no_show', 'default', 'default_with_coeff') NOT NULL default 'default',\n"
-               "banner_units int NOT NULL default 10,\n"
+               "banner_units int NOT NULL DEFAULT 10,\n"
 
-               "marketing_min_share double NOT NULL,\n"
-               "cpm_limit_share double NOT NULL default 1.0,\n"
-               "thematics_similarity_threshold double NOT NULL default 1.0,\n"
+               "marketing_min_share DOUBLE NOT NULL DEFAULT 1.0,\n"
+               "cpm_limit_share DOUBLE NOT NULL DEFAULT 1.0,\n"
+               "thematics_similarity_threshold DOUBLE NOT NULL DEFAULT 1.0,\n"
 
-               "uniq_shows_limit_coeff double NOT NULL default 1.0,\n"
+               "uniq_shows_limit_coeff DOUBLE NOT NULL DEFAULT 1.0,\n"
 
-               "settings text not null default \"\",\n"
+               "settings TEXT NOT NULL,\n"
 
                "PRIMARY KEY (id)\n"
                ")",
 
-               "INSERT INTO " + long_table + "(alias, settings) values",
+               "INSERT INTO " + long_table + "(alias, settings) VALUES",
                "(\"alias_alias\", \"{no settings}\")",
                "UPDATE " + long_table + " SET user_id = 1"
             );
